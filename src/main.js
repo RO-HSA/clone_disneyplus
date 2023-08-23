9ba97f245b5c0df0ddbc3172ece31eceab05b7da
Robert <robertdahora@gmail.com>
2023-08-23 09:30:37 -0300

    add: header; add: footer; add: faq section

--- src/main.js
+++ src/main.js
@@ -1,18 +1,53 @@
 const buttons = document.querySelectorAll('[data-tab-button]');
+const questions = document.querySelectorAll('[data-faq-question]');
+const heroSection = document.querySelector('.hero');
+const header = document.querySelector('header');
 
 document.addEventListener('DOMContentLoaded', function() {
-        
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i]. addEventListener('click', function(button) {
-            const abaAlvo = button.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            const targetTab = button.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
             escondeTodasAbas();
-            aba.classList.add('shows__list--is-active');
+            tab.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active');
+            button.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    // Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseResponse)
+    }
 });
+
+function ocultaElementosDoHeader() {
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    header.classList.remove('header--is-hidden');
+}
+
+function openOrCloseResponse(element) {
+    const classTarget = 'faq__questions__item--is-open';
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(classTarget);
+}
 
 function removeBotaoAtivo() {
     for (let i = 0; i < buttons.length; i++) {
